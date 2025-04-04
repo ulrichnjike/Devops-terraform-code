d9936248bbba46530f80ebe93c58f9738eaa2e9e
ulrichnjike <94269625+ulrichnjike@users.noreply.github.com>
2025-04-03 21:46:52 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,16 @@
 
+variable "ENVIRONMENT" {
+  
+}
+
+variable "REGION" {
+description = "This is aws region"
+  default     = "us-east-1"
+  type        = string
+}
+
+variable "INSTANCE_TYPE" {
+description = "This is aws ec2 type "
+  default = "t2.medium"
+  type        = string  
+}
