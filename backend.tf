6b3bedef9381e6cf44c49b6e6409f2c017e79a6f
ulrichnjike <94269625+ulrichnjike@users.noreply.github.com>
2025-04-03 21:34:12 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-vpc-code "
+    bucket = "terraform-vpc-code"
     key = "week10/terraform.tfstate"
     region = "us-east-1"
     encrypt = true 
-    dynamodb_table = "state-lock"
+   
   }
 }
