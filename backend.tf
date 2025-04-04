8aa436841596d858d5f626bff8c67f7ebc327080
ulrichnjike <94269625+ulrichnjike@users.noreply.github.com>
2025-04-03 21:32:05 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "week10-terraform"
+    bucket = "terraform-vpc-code "
     key = "week10/terraform.tfstate"
     region = "us-east-1"
     encrypt = true 
