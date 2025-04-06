1b56c09a807f2b07511e5a185d7ae1f6ce3f3232
ulrichnjike <ulrichnjike42.gmail.com>
2025-04-06 19:07:32 -0400

    modified variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,17 @@
-
 variable "ENVIRONMENT" {
+description = "this is ec2 name"
+  default   = "dev-ec2"
+  type      = string
 }
 
 variable "REGION" {
+description = "This is aws region"
+  default     = "us-east-1"
+  type        = string
 }
 
-variable "INSTANCE_TYPE" {  
+variable "INSTANCE_TYPE" {
+description = "This is aws ec2 type "
+  default = "t2.medium"
+  type        = string  
 }
